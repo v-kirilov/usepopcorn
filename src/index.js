c39844381c3abe7ce4819f39ee0d07dfd4bdc8d8
v-kirilov <viktor@DESKTOP-3HSFPUQ.localdomain>
2025-08-07 06:44:07 +0000

    Add all project so far

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './index.css';
+import App from './App';
+
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = React.useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was ratet {movieRating} stars</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+    {/* <StarRating onSetRating={() => {}} />
+    <StarRating
+      messages={["Terrible", "Bad", "Okay", "Good", "Excellent"]}
+      defaultRating={3} onSetRating={() => {}}
+    /> */}
+    {/* <Test></Test> */}
+  </React.StrictMode>
+);
+
+function Star() {
+  return <span>⭐</span>;
+}
