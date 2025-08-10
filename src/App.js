ee2993f518d1523808302055960da2f12691f8cd
v-kirilov <v_kirilov87@protonmail.ch>
2025-08-10 19:15:26 +0000

    Add persistent variable to track how much time a movie was rated by user before adding to list.

--- src/App.js
+++ src/App.js
@@ -178,19 +178,22 @@ function Search({ query, setQuery }) {
   //Use new hook useRef to create a reference to the input element
   const inputElement = useRef(null);
 
-  useEffect(function () {
-    function callback(e) {
-      if (document.activeElement === inputElement.current) return;
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputElement.current) return;
 
-      if (e.code === "Enter") {
-        inputElement.current.focus();
-        setQuery("");
+        if (e.code === "Enter") {
+          inputElement.current.focus();
+          setQuery("");
+        }
       }
-    }
 
-    document.addEventListener("keydown", callback);
-    return () => document.addEventListener("keydown", callback);
-  }, [setQuery]);
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
 
   return (
     <input
@@ -225,6 +228,18 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [movie, setMovie] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
+
+  const countRef = useRef(0);
+
+  useEffect(
+    function () {
+      if (userRating) {
+        countRef.current++;
+      }
+    },
+    [userRating]
+  );
+
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
   const watchedUserRating = watched.find(
     (movie) => movie.imdbID === selectedId
@@ -252,6 +267,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(" ").at(0)),
       userRating,
+      countRatingDecisions: countRef.current,
     };
     onAddWatched(newWatchedMovie);
     onCloseMovie();
